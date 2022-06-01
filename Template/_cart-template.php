81f7ff83299b05afda743f0ecd529177a1291178
Agusyanto2113 <74976807+Agusyanto2113@users.noreply.github.com>
2022-06-01 16:20:49 +0700

    Add files via upload

--- Template/_cart-template.php
+++ Template/_cart-template.php
@@ -2,7 +2,7 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         if (isset($_POST['delete-cart-submit'])){
-            $deletedrecord = $Cart->deleteCart($_POST['item_id']);
+            $deletedrecord = $Cart->deleteCartItem($_POST['item_id']);
         }
 
         // save for later
