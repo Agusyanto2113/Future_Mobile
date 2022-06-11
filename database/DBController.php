c60338756cb86a19b69671976a8814a00ce31243
Agusyanto2113 <74976807+Agusyanto2113@users.noreply.github.com>
2022-06-11 14:27:36 +0700

    Add files via upload

--- database/DBController.php
+++ database/DBController.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-define('SITEURL', 'http://localhost:8081/FUTUREMOBILE/');
+define('SITEURL', 'http://localhost:8081/Future_Mobile-main/');
 define('LOCALHOST', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
